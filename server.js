ec2f3894792a6af88f6f18d7029ea8c1f64cb30a
Travis Steel <steel.travis@gmail.com>
2016-12-17 23:51:26 -0600

    added missing sankey.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var port = 1337;
+
 app.use(express.static(__dirname));
 
-app.set('port', process.env.PORT || 1337);
+app.set('port', process.env.PORT || port);
 
 var http = require('http');
 
 http.createServer(app).listen(app.get('port'), function() {
-	console.log('Server running at http://127.0.0.1:1337/');
+	console.log('Server running at http://127.0.0.1:' + port + '/');
 });
